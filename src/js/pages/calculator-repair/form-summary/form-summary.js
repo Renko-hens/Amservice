fd7895fdb0e702712c473c815ba970d6151c4bfc
renko-hens <renko.hens@yandex.ru>
2019-12-03 21:38:04 +0300

    add breadcrumbs and cars.json

--- src/js/pages/calculator-repair/form-summary/form-summary.js
+++ src/js/pages/calculator-repair/form-summary/form-summary.js
@@ -1,5 +1,6 @@
 let categoriesButton = document.querySelectorAll('.categories__button');
 
+// Меняет цвет кнопки
 categoriesButton.forEach((button) => {
   button.addEventListener('click', () => {
     categoriesButton.forEach((item) =>{
@@ -11,3 +12,44 @@ categoriesButton.forEach((button) => {
     });
   });
 });
+
+
+// XHR
+const requestURL = 'http://localhost:8081/site/templates/mocks/repair-calculate.json';
+
+function sendRequest(method, url, body = null) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+
+    xhr.responseType = 'json'
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response)
+      } else {
+        resolve(xhr.response)
+      }
+    }
+    
+    xhr.oneerror = () => {
+      reject(xhr.response)
+    }
+    
+    xhr.send(JSON.stringify(body))
+  })
+}
+
+// sendRequest("GET", requestURL)
+//   .then( data => console.log(data))
+//   .catch( err => console.error(err))
+
+const body = {
+  id : 1,
+  name: "lel"
+}
+
+sendRequest("POST", requestURL, body)
+  .then( data => console.log(data))
+  .catch( err => console.log(err))
